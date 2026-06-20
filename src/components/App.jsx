06fefa6e7c7e5dd79bd66527843dfe77e0469a83
trivekram-s <s.trivekram@genpact.com>
2026-06-20 23:10:17 +0530

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,40 @@
-// create your App component here
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  function fetchDog() {
+    setLoading(true);
+
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    fetchDog();
+  }, []);
+
+  return (
+    <div>
+      <h1>Random Dog Generator</h1>
+
+      <button onClick={fetchDog}>
+        Get New Dog
+      </button>
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
